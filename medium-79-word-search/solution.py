528d276b7d89b8d4c96a3982ed4a823b089d500b
JongEun,Jeong | Brian <with.beth@gmail.com>
2019-05-04 01:39:42 +0900

    Revise 79-word-search #3 using clear recursion

--- medium-79-word-search/solution.py
+++ medium-79-word-search/solution.py
@@ -9,91 +9,56 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        # visited map - key : (i,j), val: bool
-        # letter2coord map key: letter, val : coord(i,j) list
-        # q
         if len(word) == 0:
             return False
-        visited = self.get_visited_map(board)
-        found_result = []
-        for current in self.letter2coord(word[0], board):
-            if not visited[current]:
-                #found_result.append(self.explore(current, word, board, visited))
-                found_result.append(self.explore_dfs(current, 0, word, board, visited))
-        return len(filter(lambda b: b is True, found_result)) > 0
+        if not board or len(board[0]) == 0:
+            return False
 
-    def explore(self, start, word, board, visited):
-        print "Start explore.."
-        found_size = 0
-        current_idx = 0
-        q = Queue()
-        q.put(start)
-        while not q.empty():
-            current = q.get()
-            if not visited[current]:
-                print "[Current:", self.coord2letter(current, board), current, "]"
-                visited[current] = True
-                map(q.put, self.get_unvisited_neighbors(current, current_idx, word, board, visited))
-                for nxt in self.get_unvisited_neighbors(current, current_idx, word, board, visited):
-                    print "[Neighbor: ", self.coord2letter(nxt, board), nxt, " ]"
-                found_size = found_size + 1
-                current_idx = current_idx + 1
-
-        return len(word) == found_size
-
-    def explore_dfs(self, current, current_idx, word, board, visited):
-        if len(word) - 1 == current_idx:
-            print "Founded!"
-            return True
-        if not visited[current]:
-            print "[Current:", self.coord2letter(current, board), current, "]"
-            visited[current] = True
-
-            for nxt in self.get_unvisited_neighbors(current, current_idx, word, board, visited):
-                print "[Neighbor: ", self.coord2letter(nxt, board), nxt, " ]"
-            for nxt in self.get_unvisited_neighbors(current, current_idx, word, board, visited):
-                if self.explore_dfs(nxt, current_idx + 1, word, board, visited):
-                    return True
-                else:
-                    print "  Make visit false to..", self.coord2letter(current, board), current
-                    print "  Make visit false to..", self.coord2letter(nxt, board), nxt
-                    visited[current] = False
-                    visited[nxt] = False
-
-
-        print "Not Founded..."
+        for start in self.letter2rc(word[0], board):
+            if self.dfs(start, word, board):
+                return True
         return False
 
-    def get_unvisited_neighbors(self, current, current_idx, word, board, visited):
-        nxt_idx = current_idx + 1
-        if nxt_idx < len(word):
-            for nxt in self.get_neighbor_candidates_coords(current):
-                if nxt in visited \
-                    and not visited[nxt] \
-                        and self.coord2letter(nxt, board) == word[nxt_idx]:
-                    yield nxt
+    def dfs(self, current_rc, remaining_letters, board):
+        if len(remaining_letters) == 0:
+            return True
+        if not self.is_in_board(current_rc, board) \
+                or not self.rc2letter(current_rc, board) == remaining_letters[0]:
+            return False
 
-    def get_neighbor_candidates_coords(self, current):
+        # Avoid visiting again
+        board[current_rc[0]][current_rc[1]] = '*'
+
+        # Sum result(True, False) of traversing sub-tree candidates
+        subtree_res = reduce(lambda t, c: t + c,
+                             map(lambda nxt: self.dfs(nxt, remaining_letters[1:], board),
+                                 self.get_subtree_candidates(current_rc)))
+        # Re-set
+        board[current_rc[0]][current_rc[1]] = remaining_letters[0]
+
+        return subtree_res
+
+    def is_in_board(self, current, board):
+        i, j = current
+        r = len(board[0])
+        c = len(board)
+        return 0 <= i < c and 0 <= j < r
+
+    def get_subtree_candidates(self, current):
         i, j = current
         return [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
 
-    def get_visited_map(self, board):
-        return {(i, j): False
-                for i, j, _
-                in self.get_coord_and_letter(board)}
-
-    def coord2letter(self, coord, board):
+    def rc2letter(self, coord, board):
         i, j = coord
         return board[i][j]
 
-    def letter2coord(self, letter, board):
-        # Or it'd be better to just had a map
-        for i, j, other_letter in self.get_coord_and_letter(board):
-            if letter == other_letter:
+    def letter2rc(self, letter, board):
+        for i, j, other in self.iter_board(board):
+            if letter == other:
                 yield (i, j)
 
-    def get_coord_and_letter(self, board):
-        for i, row in zip(xrange(len(board)), board):
-            for j, letter in zip(xrange(len(row)), row):
+    def iter_board(self, board):
+        for i in xrange(len(board)):
+            for j, letter in zip(xrange(len(board[0])), board[i]):
                 yield (i, j, letter)
 
